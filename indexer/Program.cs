b455ac7590654d44331ad1f2f58e3c3e115d8e3f
Igor Zamuragin <zamuragin03@gmail.com>
2022-02-13 22:03:37 +0300

    added indexer

---
+++ indexer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace indexer
+{
+    class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+    class Company
+    {
+        Person[] personal;
+        public Company(Person[] people) => personal = people;
+        // индексатор
+        public Person this[int index]
+        {
+            get => personal[index];
+            set => personal[index] = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var microsoft = new Company(new[]
+            {
+                new Person("Tom"), new Person("Bob"), new Person("Sam"), new Person("Alice")
+            });
+            Person firstPerson = microsoft[0];
+
+            Console.WriteLine(firstPerson.Name);  // Tom
+
+            microsoft[0] = new Person("Mike");
+
+            Console.WriteLine(microsoft[0].Name);  // Mike
+        }
+    }
+}
